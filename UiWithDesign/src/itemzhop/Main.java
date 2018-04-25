e6dbef0481630fc770ac1d1cbf56dd0110bac8cb
Unknown <mesutbunaldi@gmail.com>
2018-04-26 02:40:54 +0300

    kullanıcı kayıt sistemi, kullanıcı giriş sistemi tamamlandı.

--- UiWithDesign/src/itemzhop/Main.java
+++ UiWithDesign/src/itemzhop/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("chooseWindow.fxml"));
         primaryStage.setTitle("Project Item Store");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
