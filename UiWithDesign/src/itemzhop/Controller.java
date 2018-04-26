0333302ea2e09ef9e6a1e2747c797ac4b05a367a
Unknown <mesutbunaldi@gmail.com>
2018-04-26 20:50:22 +0300

    items.csv den çekildi list objesi içersinde gösteridi

--- UiWithDesign/src/itemzhop/Controller.java
+++ UiWithDesign/src/itemzhop/Controller.java
@@ -4,21 +4,29 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.VBox;
 
+import javax.xml.soap.Text;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Controller {
     @FXML private VBox leftBox;
-    @FXML private TableView storeTable ;
+    @FXML private TableView<Item> storeTable ;
+    private ObservableList data;
+   @FXML private ListView<Item> listView;
+
+
+    
     //storeTable içerisine items.csv içerisinde yer alan ürünlerin tamamını çekeceğiz ve her satırı bir eleman olarak ekleyeceğiz
     private void ahmetlen(ActionEvent event) {
 
@@ -27,45 +35,40 @@ public class Controller {
 
     @FXML
     public void initialize() throws IOException {
-        storeTable.setEditable(true);
-
-
-        //burada bir while düşünelim items ın data filend ı kadar tabloda kolon oluşturulsun
-
-
-        TableColumn firstNameCol = new TableColumn("First Name");
-        TableColumn lastNameCol = new TableColumn("Last Name");
-        TableColumn emailCol = new TableColumn("Email");
-        storeTable.getColumns().addAll(firstNameCol, lastNameCol, emailCol);
-        firstNameCol.setCellValueFactory(new PropertyValueFactory<
-                Item,String>("First Name"));
 
 
         BufferedReader getUserData = new BufferedReader(new FileReader("items.csv"));
         String[] tmpItemStr;
         String line;
         ArrayList<Item> myItem=new ArrayList<>( );
-        Item tempItem=new Item();
+        Item tempItem;
 
         while ((line = getUserData.readLine()) != null) {
             tmpItemStr = line.split(",");
-            tempItem.name=tmpItemStr[0].toString();
-            tempItem.description=tmpItemStr[1].toString();
+            tempItem=new Item();
+            tempItem.name=tmpItemStr[0];
+            tempItem.description=tmpItemStr[1];
             tempItem.defaultPrice=0;
             myItem.add(tempItem);
-            System.out.println(tempItem.toString());
-
-
         }
+        ObservableList data = FXCollections.observableList(myItem);
+
+        listView.setItems(data);
 
 
-
-        storeTable.getItems().add(myItem);
-        storeTable.refresh();
 
     }
 
-    public void loadItems(){
+    public void loadItems() throws IOException {
+
+
+
+
+
+
+
 
     }
+
+
 }
