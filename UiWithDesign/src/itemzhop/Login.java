d7f9483dfa7f9b2113c142a5d58fe0e90186609a
AHMET ÖZYILMAZ <aozyilmaz35@gmail.com>
2018-04-26 11:37:41 +0300

    ui ekleme
    login olduktan sonra main pencerede username ve kişiye admin mi usermı onun belli olmasını ekledim.

--- UiWithDesign/src/itemzhop/Login.java
+++ UiWithDesign/src/itemzhop/Login.java
@@ -8,11 +8,14 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 
+import javax.xml.soap.Text;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -31,7 +34,6 @@ public class Login implements Initializable{
 
     public void handleButtonAction(ActionEvent actionEvent) throws IOException {
 
-
         BufferedReader getUserData = new BufferedReader(new FileReader("accounts.csv"));
         String[] tmpUser;
         String line;
@@ -41,6 +43,16 @@ public class Login implements Initializable{
                 System.out.println("Succesfull!!");
                 Parent home_page_parent= FXMLLoader.load(getClass().getResource("mainFrame.fxml"));
                 Scene home_page_scene=new Scene(home_page_parent);
+
+                TextArea playerType = (TextArea) home_page_scene.lookup("#txt_player_type");
+                playerType.setText("Hoş Geldin " + tmpUser[2]);
+                playerType.setWrapText(true);
+
+
+                TextArea lblData = (TextArea) home_page_scene.lookup("#txt_playerInfo");
+                lblData.setText("Hoş Geldin " + txt_username.getText());
+                lblData.setWrapText(true);
+
                 Stage app_stage=(Stage)((Node) actionEvent.getSource()).getScene().getWindow();
                 app_stage.hide();
                 app_stage.setScene(home_page_scene);
